60c158b0af8e62b58dfc1ebed2a83c5da7ae7dfe
illiashenkoo <illiashenkoo@gmail.com>
2019-06-13 15:24:58 +0300

    added custom click interval

--- libraries/startask-ktx-core/src/main/java/net/codecision/startask/ktx/core/utils/ClickController.kt
+++ libraries/startask-ktx-core/src/main/java/net/codecision/startask/ktx/core/utils/ClickController.kt
@@ -2,6 +2,8 @@ package net.codecision.startask.ktx.core.utils
 
 object ClickController {
 
+    val customClickIntervalMs: Long? = null
+
     private const val MIN_CLICK_INTERVAL_MS = 500L
 
     private var lastClickTime: Long = 0L
@@ -17,8 +19,12 @@ object ClickController {
         return isClickAllowed
     }
 
-    private fun isClickAllowed(currentTime: Long): Boolean = (currentTime - lastClickTime) > MIN_CLICK_INTERVAL_MS
+    private fun isClickAllowed(currentTime: Long): Boolean = (currentTime - lastClickTime) > getClickInterval()
 
     private fun getCurrentTimeMillis(): Long = System.currentTimeMillis()
+
+    private fun getClickInterval(): Long {
+        return customClickIntervalMs ?: MIN_CLICK_INTERVAL_MS
+    }
 
 }
